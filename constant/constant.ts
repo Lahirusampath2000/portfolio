8be1be1de66e4070c02f3759ca6efd2ab6669c55
lahiru sampath <phlsampath2000@gmail.com>
2026-04-25 12:22:48 +0530

    navbar update

--- constant/constant.ts
+++ constant/constant.ts
@@ -1,12 +1,13 @@
 import { AiOutlineHome } from 'react-icons/ai'
 import { BiUser } from 'react-icons/bi'
 import { GoProject } from 'react-icons/go'
-import { MdOutlineContactMail } from 'react-icons/md'
+import { MdOutlineContactMail , MdMiscellaneousServices} from 'react-icons/md'
 import { IconType } from 'react-icons'
 
 export const NavLinks = [
   { id: 1, url: "#", label: "Home", icon: AiOutlineHome },
   { id: 2, url: "#about", label: "About", icon: BiUser },
-  { id: 3, url: "#projects", label: "Projects", icon: GoProject },
-  { id: 4, url: "#contact", label: "Contact", icon: MdOutlineContactMail },
+  { id: 3, url: "#services", label: "Services", icon: MdMiscellaneousServices },
+  { id: 4, url: "#projects", label: "Projects", icon: GoProject },
+  { id: 5, url: "#contact", label: "Contact", icon: MdOutlineContactMail },
 ]
